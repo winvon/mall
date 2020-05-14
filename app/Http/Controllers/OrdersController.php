99c5222f7c8f7a127ce511115ddae85665c8d4a4
fengwenfei <437328577@qq.com>
2020-05-14 19:16:29 +0800

    5.14

---
+++ app/Http/Controllers/OrdersController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Exceptions\InvalidRequestException;
+use App\Http\Requests\OrderRequest;
+use App\Jobs\CloseOrder;
+use App\Models\Order;
+use App\Models\ProductSku;
+use App\Models\UserAddress;
+use App\Services\CartService;
+use App\Services\OrderService;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class OrdersController extends Controller
+{
+
+    /**
+     * @param OrderRequest $request
+     * @return mixed
+     */
+    public function store(OrderRequest $request, OrderService $orderService)
+    {
+        $user    = $request->user();
+        $address = UserAddress::find($request->input('address_id'));
+
+        return $orderService->store($user, $address, $request->input('remark'), $request->input('items'));
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index(Request $request)
+    {
+        $orders = Order::query()
+            // 使用 with 方法预加载，避免N + 1问题
+            ->with(['items.product', 'items.productSku'])
+            ->where('user_id', $request->user()->id)
+            ->orderBy('created_at', 'desc')
+            ->paginate();
+
+        return view('orders.index', ['orders' => $orders]);
+    }
+
+    /**
+     * @param Order $order
+     * @param Request $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function show(Order $order, Request $request)
+    {
+        $this->authorize('own', $order);
+        return view('orders.show', ['order' => $order->load(['items.productSku', 'items.product'])]);
+    }
+}
